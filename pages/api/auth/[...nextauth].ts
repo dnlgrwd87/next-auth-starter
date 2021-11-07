097369a14d643252a936fd9547c1d42c5b6eab6b
dnlgrwd87 <dnlgrwd@gmail.com>
2021-11-07 00:43:09 -0400

    add prisma db and set logged in user id on session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,21 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import prisma from '../../../prisma';
+
 export default NextAuth({
-  jwt: {
-    secret: 'not needed when database is added',
-  },
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
-import { Center, Container } from '@chakra-ui/layout';
+import { Center, Container, Text } from '@chakra-ui/layout';
 import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const loggedInUser = session?.user;
+
   return (
     <Container>
-      <Center mt={7}>Welcome to the site!</Center>
+      <Center mt={7}>
+        {!!loggedInUser ? (
+          <pre style={{ fontSize: 12 }}>
+            {JSON.stringify(loggedInUser, null, 2)}
+          </pre>
+        ) : (
+          <Text>Log in to see your info</Text>
+        )}
+      </Center>
     </Container>
   );
 };
